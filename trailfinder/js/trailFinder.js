8b45ab75269651ed359ddcffc1e9f3ed16472304
codenametyler <codenametyler@users.noreply.github.com>
2019-10-08 21:41:24 +0400

    [REMOVE][MAPBOX GL]

--- trailfinder/js/trailFinder.js
+++ trailfinder/js/trailFinder.js
@@ -2,34 +2,30 @@ const searchStackoverflow  = (message) => encodeURI("https://stackoverflow.com/s
 
 console.log(searchStackoverflow("[JS]" + 'test'));
 
+var map = L.map('map').fitWorld();
 
-// var mymap = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoiY29kZW5hbWV0eWxlciIsImEiOiJjanYzaHYzeDUwajZiNDNvN29xczgxcWszIn0._VYJusU6Xudl9u_HMAnNDg'
+}).addTo(map);
 
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.outdoors.v11',//satellite, mapbox-terrain-v2, terrain-rgb, mapbox-streets-v8, outdoors
-//     accessToken: 'pk.eyJ1IjoiY29kZW5hbWV0eWxlciIsImEiOiJjanYzaHYzeDUwajZiNDNvN29xczgxcWszIn0._VYJusU6Xudl9u_HMAnNDg'
-// }).addTo(mymap);
+map.locate({setView: true, watch: true, maxZoom: 16});
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY29kZW5hbWV0eWxlciIsImEiOiJjanYzaHYzeDUwajZiNDNvN29xczgxcWszIn0._VYJusU6Xudl9u_HMAnNDg';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-v9',
-    zoom: 9.75,
-    center: [57.568095, -20.273942]
-});
+function onLocationFound(e) {
+	var radius = e.accuracy;
 
-map.addControl(new mapboxgl.NavigationControl());
- 
-var layerList = document.getElementById('menu');
-var inputs = layerList.getElementsByTagName('input');
- 
-function switchLayer(layer) {
-    var layerId = layer.target.id;
-    map.setStyle('mapbox://styles/mapbox/' + layerId);
+	L.marker(e.latlng).addTo(map)
+		//.bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+	//L.circle(e.latlng, radius).addTo(map);
 }
- 
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].onclick = switchLayer;
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+	alert(e.message);
 }
+
+map.on('locationerror', onLocationError);
