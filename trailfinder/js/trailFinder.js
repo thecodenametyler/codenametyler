7a5bda4bcf42f9901be11391199c702c7d701276
codenametyler <codenametyler@users.noreply.github.com>
2019-10-08 23:18:54 +0400

    [UPDATE][TRAILFINDER API]

--- trailfinder/js/trailFinder.js
+++ trailfinder/js/trailFinder.js
@@ -4,28 +4,56 @@ console.log(searchStackoverflow("[JS]" + 'test'));
 
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	id: 'mapbox.streets',
-	accessToken: 'pk.eyJ1IjoiY29kZW5hbWV0eWxlciIsImEiOiJjanYzaHYzeDUwajZiNDNvN29xczgxcWszIn0._VYJusU6Xudl9u_HMAnNDg'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-map.locate({setView: true, watch: true, maxZoom: 16});
+// map.locate({setView: true, watch: true, maxZoom: 16});
+// function onLocationFound(e) {
+// 	var radius = e.accuracy / 4;
 
-function onLocationFound(e) {
-	var radius = e.accuracy;
+// 	L.marker(e.latlng).addTo(map)
+// 		.bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-	L.marker(e.latlng).addTo(map)
-		//.bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-	//L.circle(e.latlng, radius).addTo(map);
-}
-
-map.on('locationfound', onLocationFound);
+// 	L.circle(e.latlng, radius).addTo(map);
+// }
+// map.on('locationfound', onLocationFound);
 
 function onLocationError(e) {
 	alert(e.message);
 }
 
 map.on('locationerror', onLocationError);
+
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) { // success callback
+    var crd = pos.coords;
+  
+    var values = [crd.latitude, crd.longitude];
+    doSomethingWithCoordinateValues(values);
+  };
+  
+  function doSomethingWithCoordinateValues(coords) {
+    // do something with 'coords'
+
+    var lat = coords[0];
+    var lng = coords[1];
+    var zoom = 12;
+
+    // add a marker
+    var marker = L.marker([lat, lng],{}).addTo(map);
+    
+    map.setView([lat, lng], zoom);
+  }
+  
+  function error(err) { // error callback
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
