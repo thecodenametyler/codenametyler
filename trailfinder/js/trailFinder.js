58f84b4d68fd9e30ae7c0e25fe48fb2f343bef77
ritesh <ritesh@sugar.mu>
2019-10-09 16:51:35 +0400

    [UPDATE][RESOURCES] update overlay image

--- trailfinder/js/trailFinder.js
+++ trailfinder/js/trailFinder.js
@@ -41,19 +41,19 @@ navigator.geolocation.getCurrentPosition(success, error, options);*/
 var imageBoundaries =  {
   img_1: {
     top: [-20.295813, 57.51352910000001],
-    bottom: [-22.295813, 60.51352910000001]
+    bottom: [-20.495813, 58.51352910000001]
   }
 };
 L.marker(imageBoundaries.img_1.top).addTo(map);
 L.marker(imageBoundaries.img_1.bottom).addTo(map);
-var imageUrl = 'https://www.preeska.com/assets/img/landing/illu_recycle.svg',
+var imageUrl = '../images/logo.png',
   imageBounds = [imageBoundaries.img_1.top, imageBoundaries.img_1.bottom];
 
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 L.imageOverlay(imageUrl, imageBounds).bringToFront();
 
 
-var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
     esri = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
 
 var baseMaps = {
