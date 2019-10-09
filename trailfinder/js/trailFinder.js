254cd86ffe56d1853a8f3cec65847aeab14d4cb7
ritesh <ritesh@sugar.mu>
2019-10-09 14:52:50 +0400

    [ADD][IMGE OVERLAY]

--- trailfinder/js/trailFinder.js
+++ trailfinder/js/trailFinder.js
@@ -2,7 +2,16 @@ const searchStackoverflow  = (message) => encodeURI("https://stackoverflow.com/s
 
 console.log(searchStackoverflow("[JS]" + 'test'));
 
-var map = L.map('map').fitWorld();
+var map = L.map('map').setView([-20.295813, 57.51352910000001], 10);
+
+var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    mqi = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png", {subdomains: ['otile1','otile2','otile3','otile4']});
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "MapQuestImagery": mqi
+};
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -22,38 +31,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function onLocationError(e) {
 	alert(e.message);
 }
-
 map.on('locationerror', onLocationError);
 
-
 var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-  
-  function success(pos) { // success callback
-    var crd = pos.coords;
-  
-    var values = [crd.latitude, crd.longitude];
-    doSomethingWithCoordinateValues(values);
-  };
-  
-  function doSomethingWithCoordinateValues(coords) {
-    // do something with 'coords'
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+function success(pos) { // success callback
+  var crd = pos.coords;
+  var values = [crd.latitude, crd.longitude];
+  doSomethingWithCoordinateValues(values);
+};
+function doSomethingWithCoordinateValues(coords) {
+  // do something with 'coords'
+  var lat = coords[0];
+  var lng = coords[1];
+  var zoom = 12;
+  // add a marker
+  var marker = L.marker([lat, lng],{}).addTo(map);
+  map.setView([lat, lng], zoom);
+}
+function error(err) { // error callback
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+navigator.geolocation.getCurrentPosition(success, error, options);
 
-    var lat = coords[0];
-    var lng = coords[1];
-    var zoom = 12;
-
-    // add a marker
-    var marker = L.marker([lat, lng],{}).addTo(map);
-    
-    map.setView([lat, lng], zoom);
+//Fit image in boundary
+var imageBoundaries =  {
+  img_1: {
+    top: [-20.295813, 57.51352910000001],
+    bottom: [-22.295813, 60.51352910000001]
   }
-  
-  function error(err) { // error callback
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-  };
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
+};
+L.marker(imageBoundaries.img_1.top).addTo(map);
+L.marker(imageBoundaries.img_1.bottom).addTo(map);
+var imageUrl = 'https://www.preeska.com/assets/img/landing/illu_recycle.svg',
+  imageBounds = [imageBoundaries.img_1.top, imageBoundaries.img_1.bottom];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+L.imageOverlay(imageUrl, imageBounds).bringToFront();
