921062767746463cfdbd8f0c3c01a2a5ff598e92
ritesh <ritesh@sugar.mu>
2019-10-09 16:21:34 +0400

    [CLEAN UP]

--- trailfinder/js/trailFinder.js
+++ trailfinder/js/trailFinder.js
@@ -4,29 +4,9 @@ console.log(searchStackoverflow("[JS]" + 'test'));
 
 var map = L.map('map').setView([-20.295813, 57.51352910000001], 10);
 
-var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
-    mqi = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png", {subdomains: ['otile1','otile2','otile3','otile4']});
-
-var baseMaps = {
-    "OpenStreetMap": osm,
-    "MapQuestImagery": mqi
-};
-
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-// map.locate({setView: true, watch: true, maxZoom: 16});
-// function onLocationFound(e) {
-// 	var radius = e.accuracy / 4;
-
-// 	L.marker(e.latlng).addTo(map)
-// 		.bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-// 	L.circle(e.latlng, radius).addTo(map);
-// }
-// map.on('locationfound', onLocationFound);
 
 function onLocationError(e) {
 	alert(e.message);
@@ -88,9 +68,5 @@ var overlays =  {//add any overlays here
 L.control.layers(baseMaps,overlays, {position: 'topright'}).addTo(map);
 
 // //initialize locate plugin
-// L.control.locate().addTo(map);
-
-// create control and add to map
 var lc = L.control.locate().addTo(map);
-// request location update and set location
 //lc.start();
